3e6baa3992ca3333630549d38eb371df0cca1cef
Nathan <aitumik@proton.me>
2026-06-12 14:03:50 +0300

    wip: implementing making uart_tx with better signature

--- uart/uart_tx.v
+++ uart/uart_tx.v
@@ -1,9 +1,16 @@
 module uart_tx #(
-    parameter CLK_DIV = 4
+    parameter CLK_FREQ = 50_000_000,
+    parameter BAUD_RATE = 115_200
 )(
     input wire clk,
-    input wire [7:0] data,
+    input wire rst,
     input wire send,
-    output reg tex 
+    input wire [7:0] data,
+    output reg tx,
+    output reg busy,
 );
+
+    localparam BITS_PER_BAUD = CLK_FREQ / BAUD_RATE;
+
+endmodule
 
